f6243adecc265e2a1074db906565497f4eb2ba4b
heeju01226 <heeju01226@gmail.com>
2021-03-21 22:32:20 +0900

    update

--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class GugudanMain {
 	public static void main(String[] args) {
-// 잘못된 값 입력 시 자동으로 다시 입력값을 받도록 만들 수는 없을까? -> while(true)와 if절을 사용하면 되겠네. 00을 입력하면 프로그램 중지하도록 하자.
+		// 반복실행하되, 잘못된 입력값을 받으면 출력하지 않고 다시 입력값을 받도록 할 수 없을까?
+		//-> while(true)와 if절을 사용하면 되겠네(break).00을 입력하면 프로그램 중지하도록 하자.continue로
 		while (true) {
 			System.out.println("몇 단 : ");
 			Scanner sc = new Scanner(System.in);
